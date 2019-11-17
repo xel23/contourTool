eb808707b86751be99b064369258e8af0ab4aca0
igor <sawqe1@yandex.ru>
2019-11-17 15:58:51 +0300

    update paths

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Output Management',
-            template: './index.html'
+            template: './src/index.html'
         })
     ],
     output: {
