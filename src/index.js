66303a28ecd601c5a38c385487cb2b38264b7d5f
яна <AvramenkoYana@yandex.ru>
2019-11-20 15:07:54 +0300

    brush editing updated

--- src/index.js
+++ src/index.js
@@ -104,13 +104,14 @@ window.onload = function () {
     brushButton.onclick = () => {
         setActive(brushButton);
         paper.tools = [];
-        let largeCircle, location, point_click, distance, arc;
+        let largeCircle, location, point_click, distance, arc, circle;
 
         let tool_contour = new paper.Tool();
         tool_contour.activate();
         tool_contour.minDistance = 5;
 
         tool_contour.onMouseDown = (event) => {
+            circle = true;
             location = path.contains(event.point);
             point_click = event.point;
             distance = point_click.getDistance(path.getNearestPoint(point_click)) / 2;
@@ -125,8 +126,10 @@ window.onload = function () {
         };
 
         tool_contour.onMouseDrag = (event) => {
-            largeCircle.position = event.point;
-            change_contour();
+            if (circle === true) {
+                largeCircle.position = event.point;
+                change_contour();
+            }
         };
 
         tool_contour.onMouseUp = () => {
@@ -141,8 +144,15 @@ window.onload = function () {
                 path = arc;
             } else if (intersections.length > 0 && location === false){
                 arc = path.subtract(largeCircle);
-                path.remove();
-                path = arc;
+                if (arc.segments !== undefined){
+                    path.remove();
+                    path = arc;
+                }
+                else {
+                    arc.remove();
+                    largeCircle.remove();
+                    circle = false;
+                }
             }
         }
     };
